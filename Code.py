a77d9cbb268d429a7551d9b61e542f41b5215ffc
Kirito071008 <107003946+Kirito071008@users.noreply.github.com>
2022-07-30 18:26:10 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -7,6 +7,8 @@ if level == 25:
   print("Nice! You uncloked a bow!")
   print("Next weapon at level: 100")
   
+  #you can put more elif if you need 
+  
   #if you want, you can add an another sentence
   #else:
   #print("This sentece will be shown if the level is not equal to 25 or 50")
